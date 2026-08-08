ce99436a449d2846fd30ed0eb7a6a8c6cde547ea
HARIS KRISHNAN S <harisvijay128@gmail.com>
2026-08-08 13:57:28 +0530

    Create a simple Hello World program in C
    Add a basic C program to print 'Hello World!'

---
+++ c_programming_basics/01-basics/01-hello world.c
@@ -0,0 +1,9 @@
+// import the header file for standard input and output
+#include <stdio.h>
+
+// default compiler starts in main function void means no return value
+void main()
+{
+    // print the string on the screen
+    printf("Hello World!")
+}
