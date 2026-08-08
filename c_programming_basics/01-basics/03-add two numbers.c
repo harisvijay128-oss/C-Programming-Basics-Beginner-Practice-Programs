4c6b477ded11c137af35c3fcadce64e068b20c44
HARIS KRISHNAN S <harisvijay128@gmail.com>
2026-08-08 15:01:08 +0530

    Add C program to sum two integers

---
+++ c_programming_basics/01-basics/03-add two numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b, sum;
+
+  printf("Enter two integers:");
+  scanf("%d %d", &a, &b);
+
+  sum = a + b;
+
+  printf("Sum of a and b is %d", sum);
+
+  return 0
+}
