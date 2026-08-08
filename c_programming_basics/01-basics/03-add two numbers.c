9c2306be99e089add4f677dc54f48b44107e52d6
HARIS KRISHNAN S <harisvijay128@gmail.com>
2026-08-08 20:13:09 +0530

    Fix formatting and add newline at end of file

--- c_programming_basics/01-basics/03-add two numbers.c
+++ c_programming_basics/01-basics/03-add two numbers.c
@@ -4,12 +4,13 @@ int main()
 {
   int a, b, sum;
 
-  printf("Enter two integers:");
+  printf("Enter two integers: ");
   scanf("%d %d", &a, &b);
 
+  // sum of two integer variables
   sum = a + b;
 
-  printf("Sum of a and b is %d", sum);
+  printf("\nSum of a and b is %d", sum);
 
-  return 0
+  return 0;
 }
