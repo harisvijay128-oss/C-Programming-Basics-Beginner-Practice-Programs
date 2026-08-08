52ca80e02f730bcd3e143133c43e137c69c4795f
HARIS KRISHNAN S <harisvijay128@gmail.com>
2026-08-08 14:16:00 +0530

    Implement basic integer input and output
    Fix input handling and add missing newline.

---
+++ c_programming_basics/01-basics/02-read print number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// int main function return integer value
+int main()
+{
+  // initilize the type of variable
+  int num;
+  
+  printf("Enter an integer: ");
+  // input the integer value
+  scanf("%d", num);
+  // print the input integer
+  printf("You entered integer: %d", num);
+  // return value
+  return 0
+}
